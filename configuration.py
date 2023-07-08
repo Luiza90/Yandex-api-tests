329e296402b28d607e0b3ba75ce385fe0500ba84
Luiza90 <luizamaxmudova@yandex.ru>
2023-07-08 20:27:53 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://1c075fff-9ae6-4c40-90c6-2e330c1875e9.serverhub.praktikum-services.ru"
+CREATE_USER_PATH = "/api/v1/users/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
